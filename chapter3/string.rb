783fbae38a74803dd1c5a3032bc87c2e36738e66
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 13:51:51 -0600

    number vs digit(string)

--- chapter3/string.rb
+++ chapter3/string.rb
@@ -22,3 +22,21 @@ puts 'blink ' * 4
 #I like apple pie.
 #
 #blink blink blink blink
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# OUTPUT:
+#
+#24
+#1212
+#12+12
+#
+#10
+#22222
+#2 * 5
