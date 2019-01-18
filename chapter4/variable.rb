2d88d1800a28103a061c9fb188dfe2f15348dce1
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 14:03:13 -0600

    assigning variables

---
+++ chapter4/variable.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+
+# OUTPUT:
+#
+#...you can say that again...
+#...you can say that again...
+
+name = 'Anya christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
+
+# OUTPUT:
+#
+#My name is Anya christina Emmanuella Jenkins Harris.
+#Wow! Anya christina Emmanuella Jenkins Harris
+#is a really long name!
