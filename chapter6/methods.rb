8d5b3502da1d17c33108f1297a73528d9b32312d
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 15:34:03 -0600

    center method

--- chapter6/methods.rb
+++ chapter6/methods.rb
@@ -52,3 +52,9 @@ puts letters
 #AbCdEFg
 #Abcdefg
 #aBcDefG
+
+line_width=50
+puts( 'Old Mother Hubbard'.center(line_width))
+puts( 'Sat in her cupboard'.center(line_width))
+puts( 'Eating her curds and whey,'.center(line_width))
+puts( 'And scared her poor shoe dog away'.center(line_width))
