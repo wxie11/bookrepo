f6d4e21a48ebb81b00ce434ce446ef4ade76fe78
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 15:14:47 -0600

    reverse method

--- chapter6/methods.rb
+++ chapter6/methods.rb
@@ -4,3 +4,22 @@ puts self
 # OUTPUT:
 #
 #main
+
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+var3 = '....TCELES B HSUP A magic spell?'
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+# OUTPUT:
+#
+#pots
+#stressed diaper reviled
+#?lleps cigam A PUSH B SELECT....
+#stop
+#deliver repaid desserts
+#....TCELES B HSUP A magic spell?
