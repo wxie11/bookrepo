775daf9b37296589497a3ad9aa436da81480318a
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 15:36:47 -0600

    ljust and rjust methods

--- chapter6/methods.rb
+++ chapter6/methods.rb
@@ -58,3 +58,10 @@ puts( 'Old Mother Hubbard'.center(line_width))
 puts( 'Sat in her cupboard'.center(line_width))
 puts( 'Eating her curds and whey,'.center(line_width))
 puts( 'And scared her poor shoe dog away'.center(line_width))
+
+line_width = 40
+str = '--> text <--'
+puts(str.ljust(line_width))        
+puts(str.center(line_width))
+puts(str.rjust(line_width))
+puts(str.ljust(line_width/2) + str.rjust(line_width/2) )
