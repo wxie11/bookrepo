93a070690d3cafcbef209103eae2d92d5b741149
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 14:16:26 -0600

    get the string version of an object by using .to_s

--- chapter4/variable.rb
+++ chapter4/variable.rb
@@ -18,3 +18,38 @@ puts 'is a really long name!'
 #My name is Anya christina Emmanuella Jenkins Harris.
 #Wow! Anya christina Emmanuella Jenkins Harris
 #is a really long name!
+
+composer = 'Mozart'
+puts composer + 'was "da bomb" in his day.'
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+# OUTPUT:
+#
+#Mozartwas "da bomb" in his day.
+#But I prefer Beethoven, personally.
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+my_own_var = 5 * (1+2)
+puts my_own_var
+
+var1= 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
+
+# OUTPUT:
+#
+#just another string
+#15
+#
+#8
+#8
+#(empty)
+#eight
+#8
---
+++ chapter5/mixing.rb
@@ -0,0 +1,8 @@
+#! /usr/bin/env ruby
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+
+# OUTPUT:
+#
+#25
