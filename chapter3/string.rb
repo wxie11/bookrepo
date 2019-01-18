e77e945f63883c4e20d972b928397070ec0ea151
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 13:48:30 -0600

    string arithmetic

--- chapter3/string.rb
+++ chapter3/string.rb
@@ -8,3 +8,17 @@ puts 'Good-bye.'
 #Hello, world!
 #(empty)
 #Good-bye!
+
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+# OUTPUT:
+#
+#I likeapple pie.
+#I like apple pie.
+#I like apple pie.
+#
+#blink blink blink blink
