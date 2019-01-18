69eb11abbb47c3dc52e52f7f78c971987b32efc2
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 14:23:18 -0600

    more conversions

--- chapter5/mixing.rb
+++ chapter5/mixing.rb
@@ -8,3 +8,25 @@ puts var1 + var2.to_i
 #
 #25
 #7
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+# OUTPUT:
+#
+#15.0
+#99.999
+#99
+#
+#5
+#0.0
+#
+#stringy
+#3
