4681918d59628f1ab1c1fce239f35c38b617dfcd
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 14:31:45 -0600

    gets method

--- chapter5/mixing.rb
+++ chapter5/mixing.rb
@@ -42,3 +42,8 @@ puts '20'
 #20
 
 puts gets
+
+puts 'hello there, and what\'s your name?'
+name = gets
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
