4623f3e42232c58fea738166c465bda4155e8fe4
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 20:31:53 -0600

    deaf grandma

--- chapter7/ex.rb
+++ chapter7/ex.rb
@@ -6,3 +6,16 @@ while bottles != 0
 end
   puts 'No more bottles of beer on the wall, no more bottles of beer.'
   puts 'Got to the store and buy some more, 99 bottles of beer on the wall.'
+
+  puts 'hi sonny'
+  yell = ''
+  while yell != 'BYE BYE BYE'
+    puts yell
+    yell = gets.chomp
+    if yell != yell.upcase
+      puts 'huh! speak up, sonny!'
+    elsif yell == yell.upcase && yell != 'BYE BYE BYE'
+      puts 'no! not since '+ (1930+rand(21)).to_s + "!"
+    end
+  end
+  puts 'ok bye T_T'
