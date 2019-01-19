ccd3c32f261a396b66b35d4a16b7078218fd168b
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 19:06:38 -0600

    branching

--- chapter7/methods.rb
+++ chapter7/methods.rb
@@ -25,3 +25,35 @@ puts '2'<'10'
 #true
 #true
 #false
+
+puts 'hello, what\'s your name?'
+name = gets.chomp
+puts 'hello, ' + name + '.'
+if name == 'Chris'
+  puts 'what a lovely name!'
+end
+
+puts 'I am a fortune-teller. Tell me your name:'
+name = gets.chomp
+if name =='Chris'
+  puts 'i see great things in your future.'
+else
+  puts 'your future is...oh my! look at the time!'
+  puts 'i need to go!'
+end
+
+puts 'hello, and welcome to 7 grade english'
+puts 'my name is Mrs.Gabbard. and your name is...?'
+name = gets.chomp
+if name == name.capitalize
+  puts 'please take a seat, '+ name + '.'
+else
+  puts name+ '? you mean ' + name.capitalize + ', right?'
+  puts 'don\'t you even know how to spell your name??'
+  reply = gets.chomp
+  if reply.downcase == 'yes'
+    puts 'Hmmph! well, sit down!'
+  else
+    puts 'Get out!'
+  end
+end
