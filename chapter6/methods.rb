838468442227b9c33e57b14c07a54348b56c019f
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 18:41:43 -0600

    srand method

--- chapter6/methods.rb
+++ chapter6/methods.rb
@@ -96,3 +96,11 @@ puts (rand(9999999999999999))
 puts ('The weatherman said there is a')
 puts (rand(101).to_s + '% chance of rain')
 puts (' but you can never trust a weatherman')
+
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts ''
+srand 1976
+puts(rand(100))
+puts(rand(100))
