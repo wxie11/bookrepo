1fe5b7bb8e5510d185073c618c14d1d7ded499ba
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 20:16:54 -0600

    99 bottles of beer on the wall

---
+++ chapter7/ex.rb
@@ -0,0 +1,8 @@
+bottles = 99
+while bottles != 0
+  puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
+  bottles = bottles-1
+  puts 'Take one down and pass it around, ' + bottles.to_s + ' bottles of beer on the wall.'
+end
+  puts 'No more bottles of beer on the wall, no more bottles of beer.'
+  puts 'Got to the store and buy some more, 99 bottles of beer on the wall.'
