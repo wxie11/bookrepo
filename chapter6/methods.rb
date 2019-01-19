e69a519e7011954dc47aeaaa21b7bdd7d3350b6f
wenwen xie <wxie@wenwens-iMac.local>
2019-01-18 18:45:58 -0600

    math object

--- chapter6/methods.rb
+++ chapter6/methods.rb
@@ -104,3 +104,19 @@ puts ''
 srand 1976
 puts(rand(100))
 puts(rand(100))
+
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1+Math.sqrt(5))/2)
+
+# OUTPUT:
+#
+#3.141592653589793
+#2.718281828459045
+#0.5000000000000001
+#0.9999999999999999
+#2.0
+#1.618033988749895
