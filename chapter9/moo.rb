4daf9c88b6e490f7d6c5e50c8d2453053b51484c
wenwen xie <wxie@wenwens-iMac.local>
2019-01-23 13:26:55 -0600

    add parameter

--- chapter9/moo.rb
+++ chapter9/moo.rb
@@ -1,8 +1,5 @@
-def say_moo
-  puts 'moooooo...'
+def say_moo number_of_moos
+  puts 'moooooo...'*number_of_moos
 end
-say_moo
-say_moo
-puts 'coin-coin'
-say_moo
-say_moo
+say_moo 3
+puts 'oink-oink'
