24f09b725e0ae86cca7822a74518128313d24fa1
wenwen xie <wxie@wenwens-iMac.local>
2019-01-23 13:34:14 -0600

    local var

--- chapter9/moo.rb
+++ chapter9/moo.rb
@@ -9,3 +9,11 @@ def double_this num
   puts num.to_s+' doubled is '+num_times_2.to_s
 end
 double_this 44
+
+tough_var = 'you can\'t even touch my variable!'
+def little_pest tough_var
+  tough_var = nil
+  puts 'HAHA! I ruined your variable!'
+end
+little_pest tough_var
+puts tough_var
