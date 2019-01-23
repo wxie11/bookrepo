0995133b61cb712d484c31a32d27cbe3d5ef0bd6
wenwen xie <wxie@wenwens-iMac.local>
2019-01-23 13:22:10 -0600

    writing a survey

---
+++ chapter9/ownMethods.rb
@@ -0,0 +1,48 @@
+puts 'Hello, and thank you for taking the time to'
+puts 'help me with this experiment.'
+while true
+  puts 'Do you like eating tacos?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' || answer == 'no')
+    break
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+while true
+  puts 'Do you like eating burritos?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' || answer == 'no')
+    break
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+while true
+  puts 'Do you wet the bed?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' || answer == 'no')
+    if answer == 'yes'
+      wets_bed = true
+    else
+      wets_bed = false
+    end
+    break
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+while true
+  puts 'Do you like eating chimichangas?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' || answer == 'no')
+    break
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+puts
+puts 'DEBRIEFING'
+puts 'Thank you for taking the time to help with'
+puts 'this experiment.'
+puts wets_bed
