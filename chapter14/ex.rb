599f7c0c7ae80c9f7e9979d2804afd793030c3d7
wenwen xie <wxie@wenwens-iMac.local>
2019-01-25 12:50:34 -0600

    finish

--- chapter14/ex.rb
+++ chapter14/ex.rb
@@ -11,9 +11,32 @@ def profile block_description, &block
   end
 end
 
-#Grandfather clock
+#Grandfather clock (from author)
 def grandfather_clock &block
   hour = (Time.new.hour + 11) % 12 + 1
   hour.times(&block)
 end
 grandfather_clock {puts 'Dong!'}
+
+#program logger (from author)
+$logger_depth = 0
+def log desc, &block
+  prefix = ' '*$logger_depth
+  puts prefix+"Begining #{desc.inspect}..."
+  $logger_depth+=1
+  result =blcok[]
+  $logger_depth -= 1
+  puts prefix+"...#{desc.inspect} finished, returning: #{result}"
+end
+log 'outer block' do
+  log 'some lttle block' do
+    log 'teeny-tiny block' do
+      'lots of love'.dowcase
+    end
+    7*3*2
+  end
+  log 'yet another block' do
+    '!doof naidnI evoL I'.reverse
+  end
+  '0' == '0'
+end
