b3fe5f9fcdc65803206717f7c9c7882d527d363b
wenwen xie <wxie@wenwens-iMac.local>
2019-01-24 23:27:35 -0600

    hash

--- chapter12/new.rb
+++ chapter12/new.rb
@@ -15,3 +15,26 @@ puts Time.local(2000,1,1)
 puts Time.local(1976,8,3,13,31)
 
 puts Time.gm(1955,11,5)
+
+#Hash
+dict_array = []
+dict_hash = {}
+dict_array[0] = 'candle'
+dict_array[1] = 'glasses'
+dict_array[2] = 'truck'
+dict_array[3] = 'alicia'
+dict_hash['shia-a'] ='candle'
+dict_hash['shaya'] = 'glasses'
+dict_hash['shasha'] = 'truck'
+dict_hash['sh-sha'] = 'alicia'
+dict_array.each do |word|
+  puts word
+end
+dict_hash.each do |c_word, word|
+  puts "#{c_word}: #{word}"
+end
+
+weird_hash = Hash.new
+weird_hash[12] = 'monkeys'
+weird_hash[[]] = 'emptiness'
+weird_hash[Time.new] = 'no time like this present'
