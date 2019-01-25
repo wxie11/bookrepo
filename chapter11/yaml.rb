c131a84c1472f4232e1c6b8701350cfe2ada2096
wenwen xie <wxie@wenwens-iMac.local>
2019-01-24 21:09:47 -0600

    yaml

--- chapter11/yaml.rb
+++ chapter11/yaml.rb
@@ -9,3 +9,19 @@ read_string = File.read filename
 read_array = YAML::load read_string
 puts(read_string == test_string)
 puts(read_array == test_array)
+
+#____________________________________
+def yaml_save object, filename
+  File.open filename, 'w' do |f|
+    f.write(object.to_yaml)
+  end
+end
+def yaml_load filename
+  yaml_string = File.read filename
+  YAML::load yaml_string
+end
+test_array1= ['slick shoes', 'bully blinders', 'pinchers of peril']
+filename = 'DatasGadgets.txt'
+yaml_save test_array1, filename
+read_array1 = yaml_load filename
+puts(read_array1 == test_array1)
